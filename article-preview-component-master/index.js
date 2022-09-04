d83b25926b675498cb1ea4083a932d59b3cee38b
joglidze <94470067+joglidze@users.noreply.github.com>
2022-09-04 15:02:41 +0400

    Add files via upload

---
+++ article-preview-component-master/index.js
@@ -0,0 +1,16 @@
+const shareBtn = document.querySelector(".share");
+const mobShare = document.querySelector(".mob-share");
+const shareBlock = document.querySelector(".flex");
+let num = 0;
+l
+shareBtn.addEventListener("click", share);
+mobShare.addEventListener("click", share);
+function share() {
+  if (num == 0) {
+    shareBlock.style.display = "flex";
+    num++;
+  } else {
+    shareBlock.style.display = "none";
+    num--;
+  }
+}
